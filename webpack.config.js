3f0e6276fc7fab01e9dbc0a322ddb11250bed737
Daniel Rohr <daniel.rohr@w.roteskreuz.at>
2019-06-02 18:01:41 +0200

    Add build config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: ['./src/app.ts', './src/styles.scss'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|ttf|woff2?|eot)$/,
+        use: "file-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({})
+  ]
+};
